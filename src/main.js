b91a07d0e00b6f95c92c38340dad843f18299ef7
yujunfa97 <yujunfa97@163.com>
2019-08-11 19:14:40 +0800

    demo

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+/* jshint esversion: 6 */
+import Vue from 'vue';
+import App from './App';
+import router from './router';
 //var VueResource = require('vue-resource');
 //Vue.use(VueResource);
 import VueResource from 'vue-resource';
 Vue.use(VueResource);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.http.options.emulateJSON = true;
 
 /* eslint-disable no-new */
@@ -17,4 +18,4 @@ new Vue({
   router,
   template: '<App/>',
   components: { App }
-})
+});
